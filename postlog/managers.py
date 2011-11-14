5c7bcc492a8050a32363b9faf726075e12d9ae9d
Alex <alex.schemelev@gmail.com>
2011-11-14 17:22:31 +0300

    fixed issue with numbering

--- postlog/managers.py
+++ postlog/managers.py
@@ -3,9 +3,11 @@ from django.db import models
 class ThreadManager(models.Manager):
 
     def from_request(self, request):
+        """ Returns query set of Thread objects linked with the request. """
         return self.get_query_set().filter(uuid__in=request.session.get('threads', []))
 
     def add_to_session(self, request, thread):
+        """ Links Thread with the request. """
         threads = request.session.get('threads', [])
         threads.append(thread.uuid)
         request.session['threads'] = threads
@@ -14,6 +16,10 @@ class ThreadManager(models.Manager):
 class PostManager(models.Manager):
     
     def create(self, **kwargs):
-        kwargs['number'] = self.get_query_set().filter(
-            thread=kwargs['thread']).count() + 1
+        """ Replaces native 'create' to initialize ordering number. """
+        try:
+            kwargs['number'] = self.get_query_set().filter(
+                thread=kwargs.get('thread', 0))[0].number + 1
+        except IndexError:
+            kwargs['number'] = 1
         return self.get_query_set().create(**kwargs)
